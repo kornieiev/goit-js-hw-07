0a761aa77950bf30452eaa5071c942ae96292dbd
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2023-05-21 15:48:15 +0200

    finished

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryList = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const galleryItemsMarkup = galleryItems
+  .map(
+    (img) => `
+      <li class="gallery__item">
+      <a class="gallery__link" href="${img.original}">
+        <img
+          class="gallery__image"
+          src="${img.preview}"
+          data-source="${img.original}"
+          alt="${img.description}" 
+          />
+      </a>
+      </li>
+        `
+  )
+  .join("");
+
+galleryList.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+
+galleryList.addEventListener("click", getBigImg);
+
+function getBigImg(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  const modalImg = basicLightbox.create(`
+  <img src="${evt.target.dataset.source}">
+`);
+  modalImg.show();
+
+  galleryList.addEventListener("keydown", closeBigImg);
+
+  function closeBigImg(evt) {
+    evt.preventDefault();
+    if (evt.code === "Escape" || evt.code === "Enter" || evt.code === "Space") {
+      modalImg.close();
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+console.log(gallery);
+
+const getMarkup = galleryItems
+  .map(
+    (img) => `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${img.original}">
+      <img class="gallery__image" src="${img.preview}" alt="${img.description}" />
+    </a>
+  </li>
+  `
+  )
+  .join("");
+console.log(getMarkup);
+
+gallery.insertAdjacentHTML("beforeend", getMarkup);
+
+gallery.addEventListener("click", getBigImg);
+function getBigImg(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  console.log(e);
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    captionPosition: "outside",
+  });
+}
